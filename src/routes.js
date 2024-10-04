1d071bc9c97cd1f1ef961ddb5ddcab1d2a4ffa49
vandodev <vandoaparecido@hotmail.com>
2024-10-04 07:36:27 -0300

    :pizza: :hamburger: :fries: Capturando dados da tela de registro

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import Login from "./screens/login/login.jsx";
 import Registro from "./screens/registro/registro.jsx";
 import Registro2 from "./screens/registro2/registro2.jsx";
+import Home from "./screens/home/home.jsx";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -11,15 +12,8 @@ function Routes() {
     return <NavigationContainer>
         <Stack.Navigator>
 
-            <Stack.Screen name="login" component={Login} options={{
+            <Stack.Screen name="home" component={Home} options={{
                 headerShown: false
-            }} />
-
-            <Stack.Screen name="registro" component={Registro} options={{
-                //headerShown: false
-                headerShadowVisible: false,
-                title: "",
-                headerBackTitle: "Voltar"//aparece voltar só no ios
             }} />
 
             <Stack.Screen name="registro2" component={Registro2} options={{
@@ -27,6 +21,17 @@ function Routes() {
                 headerShadowVisible: false,
                 title: "",
                 headerBackTitle: "Voltar"
+            }} />
+
+            <Stack.Screen name="registro" component={Registro} options={{
+                //headerShown: false
+                headerShadowVisible: false,
+                title: "",
+                headerBackTitle: "Voltar"
+            }} />
+
+            <Stack.Screen name="login" component={Login} options={{
+                headerShown: false
             }} />
 
         </Stack.Navigator>
--- src/screens/registro/registro.jsx
+++ src/screens/registro/registro.jsx
@@ -3,39 +3,54 @@ import { styles } from "./registro.style.js";
 import Header from "../../components/header/header.jsx";
 import TextBox from "../../components/textbox/textbox.jsx";
 import Button from "../../components/button/button.jsx";
+import { useState } from "react";
 
 
-function Registro() {
+function Registro(props) {
+
+    const [nome, setNome] = useState("");
+    const [email, setEmail] = useState("");
+    const [senha1, setSenha1] = useState("");
+    const [senha2, setSenha2] = useState("");
+
     return <>
         <View style={styles.container}>
-            <ScrollView style={styles.ScrollView}>
-                    <Header texto="Criar sua conta." />
+            <ScrollView style={styles.scrollView}>
+                <Header texto="Criar sua conta." />
 
-                    <View style={styles.formGroup}>
-                        <View style={styles.form}>
-                            <TextBox label="Nome Completo" />
-                        </View>
-
-                        <View style={styles.form}>
-                            <TextBox label="E-mail" />
-                        </View>
-
-                        <View style={styles.form}>
-                            <TextBox label="Escolha uma senha" isPassword={true} />
-                        </View>
-
-                        <View style={styles.form}>
-                            <TextBox label="Confirme a senha" isPassword={true} />
-                        </View>
-
-                        <View style={styles.form}>
-                            <Button texto="Próximo passo" />
-                        </View>
+                <View style={styles.formGroup}>
+                    <View style={styles.form}>
+                        <TextBox label="Nome Completo"
+                            onChangeText={(texto) => setNome(texto)}
+                            value={nome} />
                     </View>
-            </ScrollView>
 
+                    <View style={styles.form}>
+                        <TextBox label="E-mail"
+                            onChangeText={(texto) => setEmail(texto)}
+                            value={email} />
+                    </View>
+
+                    <View style={styles.form}>
+                        <TextBox label="Escolha uma senha" isPassword={true}
+                            onChangeText={(texto) => setSenha1(texto)}
+                            value={senha1} />
+                    </View>
+
+                    <View style={styles.form}>
+                        <TextBox label="Confirme a senha" isPassword={true}
+                            onChangeText={(texto) => setSenha2(texto)}
+                            value={senha2} />
+                    </View>
+
+                    <View style={styles.form}>
+                        <Button texto="Próximo passo"
+                            onPress={() => props.navigation.navigate("registro2")} />
+                    </View>
+                </View>
+
+            </ScrollView>
         </View>
-     
     </>
 }
 
