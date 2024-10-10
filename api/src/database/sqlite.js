89ce98b6fc005f881594121263b08c0d54b51d9c
vandodev <vandoaparecido@hotmail.com>
2024-10-10 07:13:05 -0300

    :pizza: :hamburger: :fries: Criando banco de bados

---
+++ api/src/database/sqlite.js
@@ -0,0 +1,22 @@
+import sqlite3 from "sqlite3";
+
+const SQLite = sqlite3.verbose();
+
+function execute(command, params, method = "all") {
+    return new Promise((resolve, reject) => {
+        db[method](command, params, (error, result) => {
+            if (error)
+                reject(error)
+            else
+                resolve(result);
+        });
+    });
+}
+
+const db = new SQLite.Database("./src/database/banco.db", SQLite.OPEN_READWRITE, (err) => {
+    if (err)
+        return console.log("Erro ao conectar ao banco: " + err.message);
+});
+
+
+export { db, execute }
