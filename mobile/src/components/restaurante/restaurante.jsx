be9ead333de2aa047447130d7685666fe0b02e4a
vandodev <vandoaparecido@hotmail.com>
2024-11-13 07:22:18 -0300

    :pizza: :hamburger: :fries: Listando favoritos

--- mobile/src/components/restaurante/restaurante.jsx
+++ mobile/src/components/restaurante/restaurante.jsx
@@ -2,13 +2,14 @@ import { Image, Text, TouchableOpacity, View } from "react-native";
 import { styles } from "./restaurante.style.js";
 
 function Restaurante(props) {
-    return <TouchableOpacity style={styles.restaurante} onPress={() => props.onPress()}>
+    return <TouchableOpacity style={styles.restaurante}
+        onPress={() => props.onPress(props.id_empresa)}>
         <Image source={{ uri: props.logotipo }} style={styles.logotipo} />
         <View style={styles.textos}>
             <Text style={styles.nome}>{props.nome}</Text>
             <Text style={styles.endereco}>{props.endereco}</Text>
         </View>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={() => props.onClickIcon(props.id_empresa)}>
             <Image source={props.icone} style={styles.favorito} />
         </TouchableOpacity>
     </TouchableOpacity>
--- mobile/src/screens/aba-favoritos/aba-favoritos.jsx
+++ mobile/src/screens/aba-favoritos/aba-favoritos.jsx
@@ -1,19 +1,45 @@
-import { FlatList, Image, Text, View } from "react-native";
-import { restaurantes } from "../../constants/dados.js";
+import { useEffect, useState } from "react";
+import { Alert, FlatList, Image, Text, View } from "react-native";
 import Restaurante from "../../components/restaurante/restaurante.jsx";
 import icons from "../../constants/icons.js";
 import { styles } from "./aba-favoritos.style.js";
+import api from "../../constants/api.js";
 
-function AbaFavoritos() {
+function AbaFavoritos(props) {
+
+    const [restaurantes, setRestaurantes] = useState([]);
+
+    async function LoadFavoritos() {
+
+        try {
+            const response = await api.get("/usuarios/favoritos");
+
+            if (response.data) {
+                setRestaurantes(response.data);
+            }
+        } catch (error) {
+            if (error.response?.data.error)
+                Alert.alert(error.response.data.error);
+            else
+                Alert.alert("Ocorreu um erro. Tente novamente mais tarde");
+        }
+    }
+
+    useEffect(() => {
+        LoadFavoritos();
+    }, []);
+
     return <View style={styles.container}>
         <FlatList data={restaurantes}
             keyExtractor={(restaurante) => restaurante.id}
             showsVerticalScrollIndicator={false}
             renderItem={({ item }) => {
-                return <Restaurante nome={item.nome}
+                return <Restaurante id_empresa={item.id_empresa}
+                    nome={item.nome}
                     endereco={item.endereco}
-                    logotipo={item.logotipo}
-                    icone={icons.remove} />
+                    logotipo={item.icone}
+                    icone={icons.remove}
+                />
             }}
 
             contentContainerStyle={styles.containerList}
