f38875d26da6e304497b9d7eeb9387e53c920c8b
vandodev <vandoaparecido@hotmail.com>
2024-12-05 08:18:05 -0300

    :pizza: :hamburger: :fries: Refatorando retorno da api

--- api/src/repositories/repository.empresa.js
+++ api/src/repositories/repository.empresa.js
@@ -69,28 +69,43 @@ async function ExcluirFavorito(id_usuario, id_empresa) {
 
 async function Cardapio(id_usuario, id_empresa) {
 
+    // Dados do restaurante ----
     let sql = `select case when u.id_favorito is null then 'N' else 'S' end as favorito, e.*
     from  empresa e
     left join usuario_favorito u on (u.id_empresa = e.id_empresa and u.id_usuario = ?)
     where e.id_empresa = ?`;
 
     const empresa = await execute(sql, [id_usuario, id_empresa]);
+    let retorno = empresa[0];
 
-    //-----------------
+    retorno.categorias = [];
 
-    sql = `select p.*, c.categoria
+
+    // Categorias do restuarnte ----
+    sql = `select distinct c.id_categoria, c.categoria
     from produto p
     join produto_categoria c on (c.id_empresa = p.id_empresa and c.id_categoria = p.id_categoria)
     where p.id_empresa = ?
     order by c.ordem, p.nome`;
 
-    const itens = await execute(sql, [id_empresa]);
+    const categorias_unicas = await execute(sql, [id_empresa]);
 
-    //-----------------
+    for (const cat of categorias_unicas) {
 
-    let retorno = empresa[0];
+        // Buscar produtos que pertencem a categoria do loop...
+        sql = `select p.*, c.categoria
+        from produto p
+        join produto_categoria c on (c.id_empresa = p.id_empresa and c.id_categoria = p.id_categoria)
+        where p.id_empresa = ?
+        and p.id_categoria = ?
+        order by c.ordem, p.nome`;
 
-    retorno.itens = itens;
+        const itens = await execute(sql, [id_empresa, cat.id_categoria]);
+
+        cat.itens = itens;
+
+        retorno.categorias.push(cat);
+    }
 
     return retorno;
 }
