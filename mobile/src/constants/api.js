c4a88d997f0cefe4e7d92464705732ee50582ff3
vandodev <vandoaparecido@hotmail.com>
2024-12-06 07:38:56 -0300

    :pizza: :hamburger: :fries: Refatorando mobile para acompanhar alterações na api

--- mobile/src/constants/api.js
+++ mobile/src/constants/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://192.168.0.102:3001",
+    baseURL: "http://192.168.0.100:3001",
     timeout: 10000
 });
 
--- mobile/src/screens/cardapio/cardapio.jsx
+++ mobile/src/screens/cardapio/cardapio.jsx
@@ -1,46 +1,27 @@
-import { useEffect, useState } from "react";
-import { Image, TouchableOpacity, View, Text, ScrollView } from "react-native";
+import { Image, TouchableOpacity, View, Text, ScrollView, Alert } from "react-native";
 import { styles } from "./cardapio.style.js";
-import { restaurante } from "../../constants/dados.js";
 import icons from "../../constants/icons.js";
 import Produto from "../../components/produto/produto.jsx";
+import { useEffect, useState } from "react";
 import api from "../../constants/api.js";
 
 function Cardapio(props) {
 
-    var categoriaAnterior = "";
-    const [cardapio, setCardapio] = useState({ itens: [] });
     const id_empresa = props.route.params.id_empresa;
+    const [cardapio, setCardapio] = useState({ categorias: [] });
     const [favorito, setFavorito] = useState("N");
 
     async function LoadCardapio(id) {
 
         try {
             const response = await api.get("/empresas/" + id + "/cardapio");
-            
             if (response.data) {
                 setCardapio(response.data);
-                setFavorito("N");
+                setFavorito(response.data.favorito);
             }
-           
+
         } catch (error) {
             console.log(error);
-            if (error.response?.data.error)
-                Alert.alert(error.response.data.error);
-            else
-                Alert.alert("Ocorreu um erro. Tente novamente mais tarde");
-        }
-    }
-
-    async function AddFavorito(id) {
-
-        try {
-            const response = await api.post("/empresas/" + id + "/favoritos");
-
-            if (response.data) {
-                setFavorito("S");
-            }
-        } catch (error) {
             if (error.response?.data.error)
                 Alert.alert(error.response.data.error);
             else
@@ -55,6 +36,22 @@ function Cardapio(props) {
 
             if (response.data) {
                 setFavorito("N");
+            }
+        } catch (error) {
+            if (error.response?.data.error)
+                Alert.alert(error.response.data.error);
+            else
+                Alert.alert("Ocorreu um erro. Tente novamente mais tarde");
+        }
+    }
+
+    async function AddFavorito(id) {
+
+        try {
+            const response = await api.post("/empresas/" + id + "/favoritos");
+
+            if (response.data) {
+                setFavorito("S");
             }
         } catch (error) {
             if (error.response?.data.error)
@@ -81,15 +78,17 @@ function Cardapio(props) {
 
     return <View style={styles.container}>
         <View style={styles.containerFoto}>
-        <Image source={{ uri: cardapio.foto }} style={styles.foto} resizeMode="cover" />
+            <Image source={{ uri: cardapio.foto }} style={styles.foto}
+                resizeMode="cover" />
 
-            <TouchableOpacity style={styles.containerBack} onPress={props.navigation.goBack}>
+            <TouchableOpacity style={styles.containerBack}
+                onPress={props.navigation.goBack}>
                 <Image source={icons.back2} style={styles.back} />
             </TouchableOpacity>
         </View>
 
         <View style={styles.header}>
-        <View style={styles.headerTextos}>
+            <View style={styles.headerTextos}>
                 <Text style={styles.nome}>{cardapio.nome}</Text>
                 <Text style={styles.taxa}>Taxa de entrega: {
                     new Intl.NumberFormat("pt-BR",
@@ -105,35 +104,33 @@ function Cardapio(props) {
 
         <ScrollView>
 
-        <View style={styles.location}>
+            <View style={styles.location}>
                 <Image source={icons.location} style={styles.locationImg} />
                 <Text style={styles.endereco}>
                     {cardapio.endereco} - {cardapio.bairro} - {cardapio.cidade} -
                     {cardapio.uf}
                 </Text>
-        </View>
+            </View>
 
             {
-                cardapio.itens.map((item) => {
-                    return <View key={item.id_produto} style={styles.containerProduto}>
+                cardapio.categorias.map((cat) => {
+                    return <View key={cat.id_categoria} style={styles.containerProduto}>
 
-                      {
-                            categoriaAnterior != item.categoria ?
-                                <Text style={styles.categoria}>{item.categoria}</Text>
-                                : null
+                        <Text style={styles.categoria}>{cat.categoria}</Text>
+
+                        {
+                            cat.itens.map((item) => {
+                                return <Produto key={item.id_produto}
+                                    id_produto={item.id_produto}
+                                    foto={item.icone}
+                                    nome={item.nome}
+                                    descricao={item.descricao}
+                                    valor={item.vl_produto}
+                                    onClick={ClickProduto}
+                                />
+                            })
+
                         }
-
-                        <Produto key={item.id_produto}
-                            id_produto={item.id_produto}
-                            foto={item.icone}
-                            nome={item.nome}
-                            descricao={item.descricao}
-                            valor={item.vl_produto}
-                            onClick={ClickProduto}
-                        />
-                        
-                        {categoriaAnterior = item.categoria}
-
 
                     </View>
                 })
